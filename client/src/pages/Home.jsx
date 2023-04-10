befaf71fcf4775c635f4b2c16ff5d0a0681c496d
Muhammad-I-Hasan <101440470+Muhammad-I-Hasan@users.noreply.github.com>
2023-04-10 15:42:03 -0600

    added text restriction

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import {Box, TextField} from "@mui/material"
+import { useState } from "react" 
 const Home = () => {
 
-    return (
-      <div>Home page</div>
-    ) 
+    
+  const [num, setNum] = useState();
+  const limitChar = 7;
+  const handleChange = (e) => {
+    // if (e.target.value.toString().includes("+") || e.target.value.toString().includes("-") || e.target.value.toString().includes("e") || e.target.value.toString().includes(".")) {
+    //   setNum
+    //   return;
+    // }
+    if (e.target.value.toString().length <= limitChar) {
+      setNum(e.target.value);
+    }
+  };
+
+  return (
+    <Box component="form">
+      <TextField
+        type="number"
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        onChange={(e) => handleChange(e)}
+        defaultValue={num}
+        value={num}
+        inputProps={{pattern: '[0-9]*'}}
+      />
+    </Box>
+  );
+    
 }
 
 export default Home
