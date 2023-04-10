ea7eb777842a5b7a7052fd55fd30dc66595ffd94
Sasha <kobrusevsasha318@gmail.com>
2023-04-09 19:56:49 -0700

    adawd

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,30 @@
+import { Drawer, Box, Typography, IconButton, Button } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import { useState } from "react";
+
+const Menu = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <IconButton size='large' edge='start' color='inherit' aria-label='logo' onClick={() => setIsOpen(true)}>
+        <MenuIcon />
+      </IconButton>
+    
+      <Drawer anchor='left' open={isOpen} onClose={() => setIsOpen(false)}>
+        <Box p={2} width='250px' textAlign='center'>
+          <Typography variant='h6' component='div'>
+            Menu
+          </Typography>
+        </Box>
+        <Button href="/">Home</Button>
+        <Button href="/booking">Book an Appointment</Button>
+        <Button href="/manage">Manage an Appointment</Button>
+        
+      </Drawer>
+    </>
+  ) 
+}
+  
+export default Menu
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+
+    return (
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam delectus reiciendis ut rem, eligendi, omnis ea quos quod aliquam perferendis impedit eos amet tenetur. Aliquid ab tenetur expedita dicta totam incidunt odit quo rerum modi aliquam cum porro voluptatum rem dolores architecto non quae accusamus temporibus, accusantium saepe ratione sequi impedit voluptas! Nihil necessitatibus iure molestias vitae exercitationem dolore placeat perspiciatis odit, corporis error quam beatae harum voluptatum, vero sequi non ut reiciendis eligendi excepturi maiores! Officiis at quis odio velit ducimus porro consequatur architecto itaque sapiente voluptatibus. Eaque, animi ratione! Nam esse, perferendis nostrum dolor quod molestiae labore asperiores.</div>
+    ) 
+}
+
+export default Home
