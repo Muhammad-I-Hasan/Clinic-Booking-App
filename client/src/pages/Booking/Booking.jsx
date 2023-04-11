5419ceac26ac644a9902098fb24ab3985dc54e0d
Muhammad-I-Hasan <101440470+Muhammad-I-Hasan@users.noreply.github.com>
2023-04-11 03:26:12 -0600

    booking complete (still need to post to db)

--- client/src/pages/Booking/Booking.jsx
+++ client/src/pages/Booking/Booking.jsx
@@ -1,6 +1,7 @@
 import PracButton from "../../components/PracButton"
 import "./Booking.css"
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { TextField } from "@mui/material";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 import Axios from "axios"
@@ -12,18 +13,21 @@ const Booking = () => {
   const [nurses, setNurses] = useState([]);
   const [loaded1, setLoaded1] = useState(false);
   const [loaded2, setLoaded2] = useState(false);
+  const [avail, setAvail] = useState([]);//show available times for selected date and practitioner
 
   const [selectedPrac, setSelectedPrac] = useState(null);//selected practitioner
   const [date, setDate] = useState(dayjs());//selected date
-  const [avail, setAvail] = useState([]);//show available times for selected date and practitioner
+  const [time, setTime] = useState(null);//selected date
+  const [HCN, setHCN] = useState("");//inputted hcn
+  //these four values are used for booking an appointment
 
+  //fetch functions
   const fetchDoctors = async () => {
     return await Axios.get("http://localhost:3001/doctors/");
   };
   const fetchNurses = async () => {
     return await Axios.get("http://localhost:3001/api/nurses/");
   };
-  
   const fetchBookedTimes = async () => {
     const temp = date.format("MMMM DD YYYY");
     console.log("pracid",selectedPrac)
@@ -32,29 +36,34 @@ const Booking = () => {
     return response.data;
   }
 
-  //handle click for practitioner
+  //handle click for selecting practitioner
   const selectPrac = (ID, name, type) => {
     console.log(ID, name, type)
     setSelectedPrac(ID);
     console.log(selectedPrac)
-    
+    //on click the available times should not be displayed
+    //on click the selected time should be removed
+    setAvail(null)
+    setTime(null);
   }
 
   const getAvailTimes = () => {
-    console.log("called getAvial")
+    //error checking incase they dont select a dr before chooseing a date and clicking view
+    if(selectedPrac === null)
+      return;
     const times = ["8:00 am","8:30 am","9:00 am","9:30 am","10:00 am","10:30 am",
     "11:00 am","11:30 am","12:00 pm","12:30 pm","1:00 pm","1:30 pm",
     "2:00 pm","2:30 pm","3:00 pm","3:30 pm","4:00 pm","4:30 pm"];
-    const notimes = []
-    console.log(times);
+    const bookedTimes = [];
     
     (async () => {
-      const bookedTimes = await fetchBookedTimes();
-      bookedTimes.map((e)=> {
-        notimes.push(e.Time);
+      const response = await fetchBookedTimes();
+      response.map((e)=> {
+        bookedTimes.push(e.Time);
       })
-      console.log("notime", notimes)
-      
+      console.log("notime", bookedTimes)
+      let diff = times.filter(x => !bookedTimes.includes(x));
+      setAvail(diff)
     })();
 
     
@@ -78,28 +87,65 @@ const Booking = () => {
         setLoaded2(true);
       })();
     }
-
   }, [])
 
 
   return (
-    <div>
+    <div className="bookingBody">
+      {/* temporary for displaying info */}
       {selectedPrac && <div>Selected Prac: {selectedPrac}</div>}
       {date && <div>Selected Date: {date.format("MMMM DD YYYY")}</div>}
-      <button onClick={()=>{console.log(doctors)}}>Test</button>
+      {time && <div>Selected Time: {time}</div>}
+
       <div className="pracSelect">
-          {/* {loading up all the doctors from the database} */}
-          {loaded1 && doctors.map((doctor, index) => 
-          <PracButton key={doctor.ID} name={doctor.Name} spec={doctor.Specialization} onClick={()=> selectPrac(doctor.ID, doctor.Name, doctor.Specialization)}/>)}
-          {/* {loading up all the nurses from the database} */}
-          {loaded2 && nurses.map((nurse, index) => 
-          <PracButton key={nurse.ID} name={nurse.Name} spec="Check up" onClick={()=> selectPrac(nurse.ID, nurse.Name, "Check up")}/>)}
+        {/* {loading up all the doctors from the database} */}
+        {loaded1 && doctors.map((doctor, index) => 
+          <PracButton 
+            key={doctor.ID} 
+            name={doctor.Name} 
+            spec={doctor.Specialization} 
+            onClick={() => selectPrac(doctor.ID, doctor.Name, doctor.Specialization)}/>)}
+
+        {/* {loading up all the nurses from the database} */}
+        {loaded2 && nurses.map((nurse, index) => 
+          <PracButton 
+            key={nurse.ID} 
+            name={nurse.Name} 
+            spec="Check up" 
+            onClick={()=> selectPrac(nurse.ID, nurse.Name, "Check up")}/>)}
       </div>
+
       <div className="datepicker">
-        <MobileDatePicker value={date} onChange={(e)=> {setDate(e)}} />
-        <button className="thingy" onClick={()=> {console.log("date on button press:", date.format("MMMM DD YYYY")); getAvailTimes()}}>View Available</button>
+        <MobileDatePicker 
+          value={date} 
+          onChange={(e)=> {setDate(e); setAvail(null)}}/>
+        {/* button displays available tiems once a date and dr are selected */}
+        <button className="viewTimes" onClick={()=> { getAvailTimes()}}>View Available</button>
       </div>
       
+      {/* display available times for that dr and date */}
+      {/* These can be replaced with availableBooking component made below but we dont need to dispaly dr date or room since they already have knowledge of that */}
+      <div className="availTimes">
+            {avail && avail.map((time,index) => (
+              <button className="deez" key={time} onClick={()=>setTime(time)}>{time}</button>
+            ))}
+      </div>
+
+      {/* only show input when previous inputs have been selected. Not sure if we want to keep this*/}
+      {(date && time) && 
+      <form className="subForm" onSubmit={()=>bookAppointment}>
+        <TextField 
+          label="Health Card Number" 
+          helperText="Enter a valid 7 digit HCN" 
+          value={HCN} 
+          onChange={(e)=>{setHCN(e.target.value);}} 
+          inputProps={{ 
+            inputMode: 'number', 
+            pattern: '[0-9]{7}' 
+        }} />
+        <button className="book">Book Appointment</button>
+      </form>}
+      {/* <button onClick={()=>console.log(avail)}>button</button> */}
     </div>
   ) 
 }
