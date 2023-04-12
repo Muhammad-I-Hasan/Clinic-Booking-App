12c2d50422c78d16511bedc741cdc821469d4b3d
MylesPribeg <73327969+MylesPribeg@users.noreply.github.com>
2023-04-11 21:58:34 -0600

    Added home page
    Added content to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,25 @@
+import "./Home.css"
 
 const Home = () => {
 
  
 
   return (
-    <div>
-      Home page
+    <div className="all">
+      
+      <div className="welcome">
+        <h1>Welcome to Taffy's Clinic!</h1>
+        <h2>To book or manage an appointment use the menu in the top left</h2>
+      </div>
+
+      <div className="infobox1">
+        <h2>Contact Information</h2>
+        <div className="info"> 
+          <h3>Address: 2205 51 ST SW, Calgary</h3>
+          <h3>Phone number: (403) 222-3145</h3>
+          <h3>Email: inquires@taffyclinic.com</h3>
+        </div>
+      </div>
     </div>
   );
     
